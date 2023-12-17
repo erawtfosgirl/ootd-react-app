2f4cefb8eef753c0156f06104626cb1088165658
vjuggler <vefaferzeliyeva11@gmail.com>
2023-12-17 17:35:16 +0400

    feat: added empty box

--- src/sections/cart/CartProduct.jsx
+++ src/sections/cart/CartProduct.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { CartItem } from '../../components/CartItem';
 import { clearBasket } from '../../redux/reducers/basketSlice';
+import { NavLink } from 'react-router-dom';
 
 export const CartProduct = () => {
     const dispatch = useDispatch();
@@ -20,43 +21,53 @@ export const CartProduct = () => {
     return (
         <section className="cart-products-section">
             <div className="container">
-                <div className="title col-lg-8 col-12 mb-4 d-flex justify-content-between align-items-center">
-                    <h2>My Shopping Cart</h2>
-                    <button type="button" onClick={() => dispatch(clearBasket())}>Remove All</button>
-                </div>
-                <div className="d-flex flex-wrap justify-content-between align-items-start">
-                    <div className="left col-lg-8 col-md-12 col-sm-12 col-12">
-                        {basket.map(product => (
-                            <CartItem
-                                key={product.id}
-                                id={product.id}
-                                thumbnail={product.thumbnail}
-                                name={product.name}
-                                quantity={product.quantity}
-                                price={product.price}
-                            />
-                        ))}
+                {basket.length > 0 ? (
+                    <div>
+                        <div className="title col-lg-8 col-12 mb-4 d-flex justify-content-between align-items-center">
+                            <h2>My Shopping Cart</h2>
+                            <button type="button" onClick={() => dispatch(clearBasket())}>Remove All</button>
+                        </div>
+                        <div className="d-flex flex-wrap justify-content-between align-items-start">
+                            <div className="left col-lg-8 col-md-12 col-sm-12 col-12">
+                                {basket.map(product => (
+                                    <CartItem
+                                        key={product.id}
+                                        id={product.id}
+                                        thumbnail={product.thumbnail}
+                                        name={product.name}
+                                        quantity={product.quantity}
+                                        price={product.price}
+                                    />
+                                ))}
+                            </div>
+                            <div className="right col-lg-4 col-md-12 col-sm-12 col-12 ps-lg-3 mt-lg-0 mt-md-5 mt-sm-5 mt-5">
+                                <ul>
+                                    <li>
+                                        <span>Subtotal :</span>
+                                        <span>{`${calculateSubtotal()}$`}</span>
+                                    </li>
+                                    <li>
+                                        <span>Delivery :</span>
+                                        <span>FREE</span>
+                                    </li>
+                                    <li>
+                                        <span>Total :</span>
+                                        <span>{`${calculateSubtotal()}$`}</span>
+                                    </li>
+                                </ul>
+                                <a href="#" className="col-12">
+                                    Go to checkout
+                                </a>
+                            </div>
+                        </div>
                     </div>
-                    <div className="right col-lg-4 col-md-12 col-sm-12 col-12 ps-lg-3 mt-lg-0 mt-md-5 mt-sm-5 mt-5">
-                        <ul>
-                            <li>
-                                <span>Subtotal :</span>
-                                <span>{`${calculateSubtotal()}$`}</span>
-                            </li>
-                            <li>
-                                <span>Delivery :</span>
-                                <span>FREE</span>
-                            </li>
-                            <li>
-                                <span>Total :</span>
-                                <span>{`${calculateSubtotal()}$`}</span>
-                            </li>
-                        </ul>
-                        <a href="#" className="col-12">
-                            Go to checkout
-                        </a>
-                    </div>
-                </div>
+                ) : (<div className='empty-box text-center'>
+                    <p>You don't have any products in your cart.</p>
+                    <NavLink to='/shop' className='btn btn-dark'>Continue shopping</NavLink>
+                </div>)}
+
+
+
             </div>
         </section>
 
