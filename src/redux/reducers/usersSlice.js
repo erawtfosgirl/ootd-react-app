828aa3e263ce6bf7843d63b635bed3bf905cae77
vjuggler <vefaferzeliyeva11@gmail.com>
2023-12-27 21:21:11 +0400

    feat: created usersSlice

--- src/redux/reducers/usersSlice.js
+++ src/redux/reducers/usersSlice.js
@@ -1,1 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+export const usersSlice = createSlice({
+    name: 'users',
+    initialState: {
+        users: []
+    },
+    reducers: {
+        addUser: (state, action) => {
+
+        }
+    }
+})
+
+export const { addUser } = usersSlice.actions;
+export default usersSlice.reducer;
